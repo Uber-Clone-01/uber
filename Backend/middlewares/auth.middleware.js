d71435a46973c87e2a7304fdccb1b7b2b92fc599
santron1 <sanchayk396@gmail.com>
2024-11-28 20:04:36 +0530

    first-half over

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,23 +1,72 @@
 const userModel = require('../models/user.model');
-const bcrypt = require('bcrypt');
+const captainModel = require('../models/captain.model');
+const blackListTokenModel = require('../models/blacklistToken.model');
 const jwt = require('jsonwebtoken');
 
-module.exports.authUser=async(req, res, next)=>{
-    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
-    if(!token){
-        return res.status(401).json({message:'Unauthorized'});
+const handleTokenErrors = (err, res) => {
+    if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({
+            message: 'Session expired. Please log in again.',
+            expiredAt: err.expiredAt,
+        });
+    } else if (err.name === 'JsonWebTokenError') {
+        return res.status(401).json({
+            message: 'Invalid token. Please log in again.',
+        });
+    } else {
+        console.error('JWT Error:', err);
+        return res.status(500).json({ message: 'Internal server error' });
     }
-    const isBlacklisted = await userModel.findOne({token: token});
-    if(isBlacklisted){
-        return res.status(401).json({message: 'Unauthorized'});
-    }
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await userModel.findById(decoded._id)
-        req.user = user;
-        return next();
+};
 
-    }catch(err){
-        return res.status(401).json({message: Unauthorized})
+module.exports.authUser = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized: Token not provided' });
     }
-}
+
+    try {
+        const isBlacklisted = await blackListTokenModel.findOne({ token });
+        if (isBlacklisted) {
+            return res.status(401).json({ message: 'Unauthorized: Token is blacklisted' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        return handleTokenErrors(err, res);
+    }
+};
+
+module.exports.authCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized: Token not provided' });
+    }
+
+    try {
+        const isBlacklisted = await blackListTokenModel.findOne({ token });
+        if (isBlacklisted) {
+            return res.status(401).json({ message: 'Unauthorized: Token is blacklisted' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id);
+        if (!captain) {
+            return res.status(404).json({ message: 'Captain not found' });
+        }
+
+        req.captain = captain;
+        next();
+    } catch (err) {
+        return handleTokenErrors(err, res);
+    }
+};
--- Backend/models/captain.model.js
+++ Backend/models/captain.model.js
@@ -68,12 +68,12 @@ const captainSchema = new mongoose.Schema({
 }) 
 
 captainSchema.methods.generateAuthToken = function(){
-    const token = jwt.sign({_id: this._id},process.env.JWT_SECRET,{expiresIn:'24'})
+    const token = jwt.sign({_id: this._id},process.env.JWT_SECRET,{expiresIn:'24h'})
     return token;
 }
-captainSchema.methods.camparePassword = async function (password){
-    return await bcrypt.compare(password,this._password);
 
+captainSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
 }
 captainSchema.statics.hashPassword = async function(password){
     return await bcrypt.hash(password,10);
