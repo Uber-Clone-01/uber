3bfe60ee9c05e1ddef58418e752887670fbbae92
Santron <167627757+santron1@users.noreply.github.com>
2024-12-15 21:25:28 +0530

    Update ChatBotPopUp.jsx

--- frontend/src/components/ChatBotPopUp.jsx
+++ frontend/src/components/ChatBotPopUp.jsx
@@ -32,7 +32,7 @@ const ChatBotPopup = () => {
 
         try {
             // Send user message to the backend and get bot response
-            const response = await axios.post('http://localhost:4000/bot/bot-res', {
+            const response = await axios.post('https://uber-clone-backend-t531.onrender.com/bot/bot-res', {
                 prompt: inputMessage,
             });
 
